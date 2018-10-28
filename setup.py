03de57d63ba83a40b7deb229508b81207c05a387
yoramk <yoramk2@yahoo.com>
2018-10-28 17:58:05 +0200

    first

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name='halolib',
-    version='0.13.1',
+    version='0.13.2',
     packages=['halolib', 'halolib.flask'],
     url='',
     license='',
