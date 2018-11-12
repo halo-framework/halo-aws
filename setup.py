fad276dea894b4b6e68900ffe4b75d61b3967659
yoramk <yoramk2@yahoo.com>
2018-11-12 12:27:52 +0200

    first

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ from distutils.core import setup
 # pip install halolib-1.0.1.zip
 # python setup.py sdist --formats=zip
 # \dev\python27\python setup.py build
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='halolib',
     version='0.13.6',
     packages=['halolib', 'halolib.flask'],
-    url='',
+    url='https://github.com/yoramk2/halolib',
     license='MIT License',
     author='yoramk2',
     author_email='yoramk2@yahoo.com',
     description='this is the Halo framework library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     zip_safe=False,
     classifiers=[
         'Environment :: Console',
