971c87d3efc3c26c83d355d7317d2db1b8f51eb8
yoramk <yoramk2@yahoo.com>
2018-11-12 16:36:27 +0200

    first

---
+++ build/lib/halolib/const.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import logging
+from enum import Enum
+
+logger = logging.getLogger(__name__)
+
+
+class HTTPChoice(Enum):  # A subclass of Enum
+    get = "GET"
+    post = "POST"
+    put = "PUT"
+    delete = "DELETE"
+    patch = "PATCH"
