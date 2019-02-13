b24f51cd451a855db4b945a664b6ed7aa39e0def
yoramk <yoramk2@yahoo.com>
2019-02-13 14:55:02 +0200

    flask

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='halo_flask',
-    version='0.15.4',
+    version='0.15.5',
     packages=['halo_flask', 'halo_flask.flask'],
     url='https://github.com/yoramk2/halo_flask',
     license='MIT License',
