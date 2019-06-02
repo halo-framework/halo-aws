e3f79e7de3d6567b3a1fa0b48b1fc52329f3a77b
yoramk <yoramk2@yahoo.com>
2019-06-02 18:40:33 +0300

    flask

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 # python setup.py sdist --formats=zip
 # python setup.py sdist bdist_wheel
-# twine upload dist/halolib-0.13.8.tar.gz -r pypitest
+# twine upload dist/halo_flask-0.13.8.tar.gz -r pypitest
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
